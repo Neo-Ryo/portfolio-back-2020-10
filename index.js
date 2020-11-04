e7013e5d5d26ff06cb21ca490b44dd8f6809b176
Neo-Ryo <marco.sch4064@gmail.com>
2020-11-04 17:34:44 +0100

    new try with cors setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var corsOptions = {
     optionsSuccessStatus: 200 
   }
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
